13b62084b0d9a597fe5c270a33a2367c7df12547
Marco-Gagliano <brazil_94@hotmail.it>
2022-07-13 17:07:20 +0200

    collegamento-css-tra-frontend-e-backend

--- public/js/app.js
+++ public/js/app.js
@@ -49544,10 +49544,21 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
-/***/ "./resources/sass/app.scss":
-/*!*********************************!*\
-  !*** ./resources/sass/app.scss ***!
-  \*********************************/
+/***/ "./resources/sass/admin/app.scss":
+/*!***************************************!*\
+  !*** ./resources/sass/admin/app.scss ***!
+  \***************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
+/***/ "./resources/sass/front/style.scss":
+/*!*****************************************!*\
+  !*** ./resources/sass/front/style.scss ***!
+  \*****************************************/
 /*! no static exports found */
 /***/ (function(module, exports) {
 
@@ -49556,14 +49567,15 @@ __webpack_require__.r(__webpack_exports__);
 /***/ }),
 
 /***/ 0:
-/*!*************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
-  \*************************************************************/
+/*!*****************************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/admin/app.scss ./resources/sass/front/style.scss ***!
+  \*****************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Brazi\Desktop\BOOLEAN\LARAVEL\laravel-many-to-many\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Brazi\Desktop\BOOLEAN\LARAVEL\laravel-many-to-many\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Brazi\Desktop\BOOLEAN\LARAVEL\laravel-api\resources\js\app.js */"./resources/js/app.js");
+__webpack_require__(/*! C:\Users\Brazi\Desktop\BOOLEAN\LARAVEL\laravel-api\resources\sass\admin\app.scss */"./resources/sass/admin/app.scss");
+module.exports = __webpack_require__(/*! C:\Users\Brazi\Desktop\BOOLEAN\LARAVEL\laravel-api\resources\sass\front\style.scss */"./resources/sass/front/style.scss");
 
 
 /***/ })
