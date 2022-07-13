1807399ae7350a53a9649945f7bdce15b3ce38d6
Marco-Gagliano <brazil_94@hotmail.it>
2022-07-13 17:37:29 +0200

    collegamento-js-frontend-e-backend

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,6 +11,7 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.js('resources/js/admin.js', 'public/js')
+mix.js('resources/js/front.js', 'public/js')
     .sass('resources/sass/admin/app.scss', 'public/css/admin')
     .sass('resources/sass/front/style.scss', 'public/css/front');
